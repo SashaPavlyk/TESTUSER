1f9f82fb2c5369e8dc10c52b8885f8d0165bb472
Vitalii <nifantievv@gmail.com>
2019-11-25 14:53:32 +0200

    Change color

--- WFAPP/WFAPP/Form1.Designer.cs
+++ WFAPP/WFAPP/Form1.Designer.cs
@@ -70,6 +70,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
